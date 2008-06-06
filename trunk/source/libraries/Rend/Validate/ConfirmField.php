811e26df27e3261973d5a935802d7015acca5497
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-06-06 01:16:32 +0000

    Added new validations

---
+++ trunk/source/libraries/Rend/Validate/ConfirmField.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Rend
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://rend.superglobals.com/license/new-bsd
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to justin.hendrickson+rend@gmail.com so I can send you a copy immediately.
+ *
+ * @category    Rend
+ * @package     Validate
+ * @copyright   2008 Justin Hendrickson
+ * @license     http://rend.superglobals.com/license/new-bsd    New BSD License
+ * @link        http://rend.superglboals.com/
+ * @since       1.0.0
+ * @version     $Id$
+ */
+
+/** Zend_Validate_Abstract */
+require_once 'Zend/Validate/Abstract.php';
+
+/**
+ * ConfirmField validation
+ *
+ * Compares the current field's value with a different supplied field
+ *
+ * @category    Rend
+ * @package     Validate
+ */
+class Rend_Validate_ConfirmField extends Zend_Validate_Abstract
+{
+
+    /**
+     *
+     */
+    const NOT_MATCH = 'notMatch';
+
+    /**
+     * @var array
+     */
+    protected $_messageTemplates = array(
+        self::NOT_MATCH => "This value does not match the '%field%' field"
+    );
+
+    /**
+     * Field to match against
+     *
+     * @var string
+     */
+    protected $_field;
+
+    /**
+     * Returns the field option
+     *
+     * @return string
+     */
+    public function getField()
+    {
+        return $this->_field;
+    }
+
+    /**
+     * Sets the field option
+     *
+     * @param  string $field
+     * @return Rend_Validate_ConfirmField
+     */
+    public function setField($field)
+    {
+        $this->_field = $field;
+        return $this;
+    }
+
+    /**
+     * Defined by Zend_Validate_Interface
+     *
+     * Returns true if and only if $value matches against the field option's value
+     *
+     * @param  string $value
+     * @throws Zend_Validate_Exception if there is a fatal error in field matching
+     * @return boolean
+     */
+    public function isValid($value, $context = null)
+    {
+        $value = $value;
+        
+        $this->_setValue($value);
+
+        if (is_array($context)) {
+            if (isset($context[$this->_field])
+                && ($value == $context[$this->_field]))
+            {
+                return true;
+            }
+        } elseif (is_string($context) && ($value == $context)) {
+            return true;
+        }
+
+        $this->_error(self::NOT_MATCH);
+        return false;
+    }
+}
