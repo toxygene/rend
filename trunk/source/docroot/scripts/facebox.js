dbc52b602474402b294c02ab0cabee8a62489a3c
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-06-27 20:04:06 +0000

    Cleaned up the css and js for debug handling

--- trunk/source/docroot/scripts/facebox.js
+++
@@ -1,9 +0,0 @@
-$(enableDebugFacebox)
-
-function enableDebugFacebox() {
-    $('#debug_exception').hide();
-    $('a[rel*=facebox]').facebox({
-        loadingImage : baseUrl + '/facebox/loading.gif',
-        closeImage   : baseUrl + '/facebox/closelabel.gif'
-    });
-}
