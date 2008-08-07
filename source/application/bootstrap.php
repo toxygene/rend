1188e484d7ce30681784ca153c2aba76cc4dc29a
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-08-07 19:24:21 +0000

    Added path settings and updated all things dependent on that path

--- source/application/bootstrap.php
+++ source/application/bootstrap.php
@@ -1,17 +1,18 @@
 <?php
-$rootPath = dirname(dirname(__FILE__));
+define('ROOT_PATH', dirname(dirname(__FILE__));
+define('APP_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'application');
 
 // Setup error reporting
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', true);
 ini_set('log_errors', true);
-ini_set('error_log', "{$rootPath}/data/logs/phperrors.log");
+ini_set('error_log', ROOT_PATH . '/data/logs/phperrors.log');
 
 // Setup include path
 set_include_path(
     get_include_path() .
     PATH_SEPARATOR .
-    "{$rootPath}/library"
+    ROOT_PATH . '/library'
 );
 
 /** Zend_Loader */
@@ -25,5 +26,7 @@ date_default_timezone_set($application->getConfig()->timezone);
 Zend_Controller_Action_HelperBroker::addPrefix('Rend_Controller_Action_Helper');
 
 Zend_Controller_Front::getInstance()
-                     ->setMode($_SERVER['REND_MODE']);
-                     ->setPath($rootPath)
+                     ->addControllerDirectory(APP_PATH . '/controllers', 'default')
+                     ->addModuleDirectory(APP_PATH . '/modules')
+                     ->setMode($_SERVER['REND_MODE'])
+                     ->setPath(ROOT_PATH);
