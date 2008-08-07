37d6499760b53945aaff556d9918f9ec6b945fa3
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-08-07 18:32:37 +0000

    Moved bootstrapping to the new location

--- trunk/source/docroot/index.php
+++ trunk/source/docroot/index.php
@@ -1,18 +1,2 @@
 <?php
-$rendPath = dirname(dirname(__FILE__));
-
-include 'common.php';
-
-$front->setPath($rendPath);
-
-if (isset($_SERVER['REND_MODE'])) {
-    $front->setMode($_SERVER['REND_MODE']);
-}
-
-ini_set('display_errors', $front->getConfig()->display_errors);
-date_default_timezone_set($front->getConfig()->timezone);
-
-include 'setup.php';
-
-$front->addModuleDirectory($rendPath . '/application')
-      ->dispatch();
+include '../application/bootstrap.php';
