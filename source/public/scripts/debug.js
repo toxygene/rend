0523eb6583a308f8572da46a377b484822be6825
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-09-14 00:46:54 +0000

    Removed various js files

--- source/public/scripts/debug.js
+++
@@ -1,7 +0,0 @@
-$(enableDebugFacebox)
-
-function enableDebugFacebox() {
-    $('#debug_complete').hide()
-                        .before('<a href="#debug_complete" rel="facebox">(Click here to show/hide the exception)</a>');
-    $('a[rel*=facebox]').facebox();
-}
