8c2b8fccea4788b0958d70a53ceeedd59dda288d
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-09-14 02:41:34 +0000

    Fixed url to the copyright

--- source/library/Rend/Controller/Action/Exception/Auth.php
+++ source/library/Rend/Controller/Action/Exception/Auth.php
@@ -5,7 +5,7 @@
  * This source file is subject to the new BSD license that is bundled
  * with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://rend.superglobals.com/license/new-bsd
+ * http://www.rendframework.com/license/new-bsd
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to justin.hendrickson+rend@gmail.com so I can send you a copy immediately.
@@ -13,8 +13,8 @@
  * @category    Rend
  * @package     Controller
  * @copyright   2008 Justin Hendrickson
- * @license     http://rend.superglobals.com/license/new-bsd    New BSD License
- * @link        http://rend.superglboals.com/
+ * @license     http://www.rendframework.com/license/new-bsd    New BSD License
+ * @link        http://www.rendframework.com/
  * @since       1.0.0
  * @version     $Id$
  */
--- source/library/Rend/Controller/Action/Helper/LayoutSelector.php
+++ source/library/Rend/Controller/Action/Helper/LayoutSelector.php
@@ -7,15 +7,15 @@
  * This source file is subject to the new BSD license that is bundled
  * with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://rend.superglobals.com/License
+ * http://www.rendframework.com/License
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to justin.hendrickson+rend@gmail.com so I can send you a copy immediately.
  *
  * @category    Rend
  * @package     Controller
- * @copyright   Copyright (c) 2008 Justin Hendrickson (http://rend.superglobals.com)
- * @license     http://rend.superglobals.com/License New BSD License
+ * @copyright   Copyright (c) 2008 Justin Hendrickson (http://www.rendframework.com)
+ * @license     http://www.rendframework.com/License New BSD License
  * @version     $Id$
  */
 
