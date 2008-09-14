0628404c94cf7dfa20f3b018d610bb9543343f32
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-09-14 02:43:07 +0000

    Updated uri to license

--- source/library/Rend/Filter/Null.php
+++ source/library/Rend/Filter/Null.php
@@ -5,7 +5,7 @@
  * This source file is subject to the new BSD license that is bundled
  * with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://www.rendframework.com/license/new-bsd
+ * http://www.rendframework.com/license.html
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to justin.hendrickson+rend@gmail.com so I can send you a copy immediately.
@@ -13,7 +13,7 @@
  * @category    Rend
  * @package     Filter
  * @copyright   2008 Justin Hendrickson
- * @license     http://www.rendframework.com/license/new-bsd    New BSD License
+ * @license     http://www.rendframework.com/license.html    New BSD License
  * @link        http://www.rendframework.com/
  * @since       1.0.0
  * @version     $Id$
