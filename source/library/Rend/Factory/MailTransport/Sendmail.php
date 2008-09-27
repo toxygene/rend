4a605eac912622d316e0f14884a12e247ecd5f00
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-09-27 17:22:44 +0000

    Updates for mail transports

--- source/library/Rend/Factory/MailTransport/Sendmail.php
+++ source/library/Rend/Factory/MailTransport/Sendmail.php
@@ -6,13 +6,16 @@
 /** Rend_Factory_Abstract */
 require_once 'Rend/Factory/Abstract.php';
 
+/** Rend_Factory_MailTransport_Interface */
+require_once 'Rend/Factory/MailTransport/Interface.php';
+
 /** Zend_Mail_Transport_Sendmail */
 require_once 'Zend/Mail/Transport/Sendmail.php';
 
 /**
  *
  */
-class Rend_Factory_Mail_Transport_Sendmail extends Rend_Factory_Abstract
+class Rend_Factory_MailTransport_Sendmail extends Rend_Factory_Abstract implements Rend_Factory_MailTransport_Interface
 {
 
     /**
