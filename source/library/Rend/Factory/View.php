b576573bb3637510660b9ea8eb5277ad0d0a0144
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-09-27 17:12:42 +0000

    Updates

--- source/library/Rend/Factory/View.php
+++ source/library/Rend/Factory/View.php
@@ -6,21 +6,128 @@
 /** Rend_Factory_Abstract */
 require_once 'Rend/Factory/Abstract.php';
 
+/** Rend_Factory_View_Interface */
+require_once 'Rend/Factory/View/Interface.php';
+
 /** Zend_View */
 require_once 'Zend/View.php';
 
 /**
  *
  */
-class Rend_Factory_View extends Rend_Factory_Abstract
+class Rend_Factory_View extends Rend_Factory_Abstract implements Rend_Factory_View_Interface
 {
+
+    /**
+     *
+     */
+    protected $_basePath;
+
+    /**
+     *
+     */
+    protected $_doctype;
+
+    /**
+     *
+     */
+    protected $_encoding;
+
+    /**
+     *
+     */
+    protected $_escape;
+
+    /**
+     *
+     */
+    protected $_filter;
+
+    /**
+     *
+     */
+    protected $_filterPath;
+
+    /**
+     *
+     */
+    protected $_helperPath;
+
+    /**
+     *
+     */
+    protected $_scriptPath;
+
+    /**
+     *
+     */
+    protected $_strictVars;
 
     /**
      *
      */
     public function create()
     {
-        return new Zend_View();
+        $config = array();
+
+        if ($this->_basePath) {
+            $config['basePath'] = $this->_basePath;
+        }
+
+        if ($this->_encoding) {
+            $config['encoding'] = $this->_encoding;
+        }
+
+        if ($this->_escape) {
+            $config['escape'] = $this->_escape;
+        }
+
+        if ($this->_filter) {
+            $config['filter'] = $this->_filter;
+        }
+
+        if ($this->_filterPath) {
+            $config['filterPath'] = $this->_filterPath;
+        }
+
+        if ($this->_helperPath) {
+            $config['helperPath'] = $this->_helperPath;
+        }
+
+        if ($this->_scriptPath) {
+            $config['scriptPath'] = $this->_scriptPath;
+        }
+
+        if ($this->_strictVars) {
+            $config['strictVars'] = $this->_strictVars;
+        }
+
+        $view = new Zend_View($config);
+
+        if ($this->_doctype) {
+            $view->getHelper('Doctype')
+                 ->setDoctype($this->_doctype);
+        }
+
+        return $view;
+    }
+
+    /**
+     *
+     */
+    public function setDoctype($doctype)
+    {
+        $this->_doctype = $doctype;
+        return $this;
+    }
+
+    /**
+     *
+     */
+    public function setEncoding($encoding)
+    {
+        $this->_encoding = $encoding;
+        return $this;
     }
 
 }
