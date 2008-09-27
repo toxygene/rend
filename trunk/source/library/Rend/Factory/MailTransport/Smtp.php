74e120cf2fcf5c66c2302b6c1d84a248d9e8dc27
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-09-27 17:22:44 +0000

    Updates for mail transports

--- trunk/source/library/Rend/Factory/MailTransport/Smtp.php
+++ trunk/source/library/Rend/Factory/MailTransport/Smtp.php
@@ -6,14 +6,27 @@
 /** Rend_Factory_Abstract */
 require_once 'Rend/Factory/Abstract.php';
 
+/** Rend_Factory_MailTransport_Interface */
+require_once 'Rend/Factory/MailTransport/Interface.php';
+
 /** Zend_Mail_Transport_Smtp */
 require_once 'Zend/Mail/Transport/Smtp.php';
 
 /**
  *
  */
-class Rend_Factory_Mail_Transport_Smtp extends Rend_Factory_Abstract
+class Rend_Factory_Mail_Transport_Smtp extends Rend_Factory_Abstract implements Rend_Factory_MailTransport_Interface
 {
+
+    /**
+     *
+     */
+    protected $_host;
+
+    /**
+     *
+     */
+    protected $_options;
 
     /**
      * Get a mail transport object
@@ -23,34 +36,14 @@ class Rend_Factory_Mail_Transport_Smtp extends Rend_Factory_Abstract
     public function create()
     {
         return new Zend_Mail_Transport_Smtp(
-            $this->getHost(),
-            $this->getOptions()
+            $this->_host,
+            $this->_options
         );
     }
 
     /**
-     * Get the host name
-     *
-     * @return  string
-     */
-    public function getHost()
-    {
-        return $this->_host;
-    }
-
-    /**
-     * Get the options
-     *
-     * @return  array
-     */
-    public function getOptions()
-    {
-        return $this->_options;
-    }
-
-    /**
      * Set the host name
-     * 
+     *
      * @param   string  $host
      * @return  Zend_Mail_Transport_Smtp
      */
