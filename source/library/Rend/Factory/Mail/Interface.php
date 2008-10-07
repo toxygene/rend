3f4b4f63e3155b5e3f9215fdd40aa70e8290536a
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-10-07 19:37:49 +0000

    Added extra info

--- source/library/Rend/Factory/Mail/Interface.php
+++ source/library/Rend/Factory/Mail/Interface.php
@@ -9,13 +9,13 @@ require_once 'Rend/Factory/Interface.php';
 /**
  *
  */
-interface Rend_Factory_Mail_Transport_Interface extends Rend_Factory_Interface
+interface Rend_Factory_Mail_Interface extends Rend_Factory_Interface
 {
 
     /**
      * Create method for factory
      *
-     * @return  Zend_Mail_Transport_Abstract
+     * @return  Zend_Mail
      */
     public function create();
 
