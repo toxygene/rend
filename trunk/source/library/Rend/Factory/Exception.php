9e01cc232cad0465a0c41033b437776fe21c1844
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-10-09 02:50:58 +0000

    Added exception classes and fixed errors with loading

---
+++ trunk/source/library/Rend/Factory/Exception.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *
+ */
+
+/**
+ *
+ */
+class Rend_Factory_Exception extends Exception
+{
+}
---
+++ trunk/source/library/Rend/Factory/MailTransport/Exception.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ */
+
+/** Rend_Factory_Exception */
+require_once 'Rend/Factory/Exception.php';
+
+/**
+ *
+ */
+class Rend_Factory_MailTransport_Exception extends Rend_Factory_Exception
+{
+}
