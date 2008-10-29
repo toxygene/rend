7cd44a9b5a42fcdba06dda55c7e91886cbe95882
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-10-29 03:10:45 +0000

    Added tests for the random string class

--- trunk/tests/Rend/RandomTest.php
+++ trunk/tests/Rend/RandomTest.php
@@ -1,21 +1,53 @@
 <?php
 /**
+ * Rend
  *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.rendframework.com/license.html
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to justin.hendrickson+rend@gmail.com so I can send you a copy immediately.
+ *
+ * @category    Rend
+ * @subpackage 	UnitTest
+ * @copyright   2008 Justin Hendrickson
+ * @license     http://www.rendframework.com/license.html    New BSD License
+ * @link        http://www.rendframework.com/
+ * @since       1.0.0
+ * @version     $Id: Random.php 183 2008-09-14 02:43:07Z justin.hendrickson $
  */
 
 /**
- *
+ * @category    Rend
+ * @subpackage 	UnitTest
  */
 class Rend_RandomTest extends PHPUnit_Framework_TestCase
 {
 
-    public function testSomething()
+    private $_random;
+
+    public function setUp()
+    {
+        $this->_random = new Rend_Random();
+    }
+
+    public function testStringsOfArbitraryLengthCanBeGenerated()
+    {
+        $this->assertEquals(
+            5,
+            strlen($this->_random->getString(5))
+        );
+    }
+
+    public function testStringsFromArbitraryHaystacksCanBeGenerated()
     {
         $random = new Rend_Random();
-        
+
         $this->assertEquals(
-            8,
-            strlen($random->getString())
+            'a',
+            $random->getString(1, 'a')
         );
     }
 
