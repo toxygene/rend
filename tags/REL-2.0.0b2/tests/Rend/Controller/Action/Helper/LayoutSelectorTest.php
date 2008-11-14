dd792e919e1f79406ab5064899ff202c9c9e599c
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-11-14 19:13:20 +0000

    Tag of 2.0.0 beta 2 release.

---
+++ tags/REL-2.0.0b2/tests/Rend/Controller/Action/Helper/LayoutSelectorTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Rend
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.rendframework.com/license.html
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to justin.hendrickson+rend@gmail.com so I can send you a copy immediately.
+ *
+ * @category    Rend
+ * @subpackage 	UnitTest
+ * @copyright   2008 Justin Hendrickson
+ * @license     http://www.rendframework.com/license.html    New BSD License
+ * @link        http://www.rendframework.com/
+ * @since       1.0.0
+ * @version     $Id$
+ */
+
+/** Rend_Controller_Action_Helper_LayoutSelector */
+require_once "Rend/Controller/Action/Helper/LayoutSelector.php";
+
+/**
+ * @category    Rend
+ * @subpackage 	UnitTest
+ */
+class Rend_Controller_Action_Helper_LayoutSelectorTest extends PHPUnit_Framework_TestCase
+{
+}
---
+++ tags/REL-2.0.0b2/tests/Rend/TestHelper.php
@@ -0,0 +1,20 @@
+<?php
+date_default_timezone_set("America/Chicago");
+
+$root = dirname(dirname(dirname(__FILE__))) . "/source";
+
+// Setup include path
+set_include_path(
+    get_include_path() .
+    PATH_SEPARATOR .
+    "{$root}/library"
+);
+
+/** Zend_Loader */
+require_once "Zend/Loader.php";
+Zend_Loader::registerAutoload();
+
+PHPUnit_Util_Filter::addDirectoryToWhiteList(
+    "{$root}/library/Rend",
+    ".php"
+);
