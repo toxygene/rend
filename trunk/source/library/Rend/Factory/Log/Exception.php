298d92e6a61c52d983b30a0481ed433573bf3d9d
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-12-20 22:28:23 +0000

    Fixed errors with log factory and added a new test and helper file

---
+++ trunk/source/library/Rend/Factory/Log/Exception.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ */
+
+/** Rend_Factory_Exception */
+require_once "Rend/Factory/Exception.php";
+
+/**
+ *
+ */
+class Rend_Factory_Log_Exception extends Rend_Factory_Exception
+{}
