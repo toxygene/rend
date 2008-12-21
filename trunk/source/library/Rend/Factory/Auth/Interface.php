fd31492878f19f352d15f47e32fd59cff2bf3870
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-12-21 00:43:33 +0000

    Added missing auth interface

---
+++ trunk/source/library/Rend/Factory/Auth/Interface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *
+ */
+
+/**
+ *
+ */
+interface Rend_Factory_Auth_Interface
+{
+
+    /**
+     * Create an authentication object
+     *
+     * @return Zend_Auth
+     */
+    public function create();
+
+}
