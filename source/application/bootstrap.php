4950b9afe29a7c283a60b17ac6e42aab8b77241d
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2008-12-29 16:07:07 +0000

    Move all bootstrap code back into the bootstrap file, removed the plugin

--- source/application/bootstrap.php
+++ source/application/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 if (isset($bootstrap) && $bootstrap) {
-    // Setup include path
+    error_reporting(E_ALL | E_STRICT);
+    ini_set("error_log", "../data/logs/phperrors.log");
+    ini_set("log_errors", true);
+
     set_include_path(
         get_include_path() .
         PATH_SEPARATOR .
@@ -12,11 +15,67 @@ if (isset($bootstrap) && $bootstrap) {
     Zend_Loader::registerAutoload();
 }
 
-Zend_Controller_Front::getInstance()
-                     ->addControllerDirectory("../application/controllers", "default")
-                     ->addModuleDirectory("../application/modules");
+/** Zend_Config_Ini */
+require_once "Zend/Config/Ini.php";
 
-$setup = new Rend_Controller_Plugin_Setup($_SERVER["REND_MODE"]);
+$config = new Zend_Config_Ini(
+    "../application/configs/config.ini",
+    $_SERVER["REND_MODE"]
+);
 
-Zend_Controller_Front::getInstance()
-                     ->registerPlugin($setup, 0);
+ini_set("display_errors", $config->displayErrors);
+
+date_default_timezone_set($config->timezone);
+
+/** Zend_Controller_Action_HelperBroker */
+require_once "Zend/Controller/Action/HelperBroker.php";
+
+Zend_Controller_Action_HelperBroker::addPrefix(
+    "Rend_Controller_Action_Helper"
+);
+
+/** Rend_Factory_View */
+require_once "Rend/Factory/View.php";
+
+if (isset($config->view)) {
+    $viewFactory = new Rend_Factory_View($config->view);
+} else {
+    $viewFactory = new Rend_Factory_View();
+}
+
+/** Zend_Controller_Action_Helper_ViewRenderer */
+require_once "Zend/Controller/Action/Helper/ViewRenderer.php";
+
+Zend_Controller_Action_HelperBroker::addHelper(
+    new Zend_Controller_Action_Helper_ViewRenderer(
+        $viewFactory->create()
+    )
+);
+
+/** Zend_Controller_Front */
+require_once "Zend/Controller/Front.php";
+
+$front = Zend_Controller_Front::getInstance();
+
+/** Rend_FactoryLoader */
+require_once "Rend/FactoryLoader.php";
+
+$factoryLoader = new Rend_FactoryLoader(
+    $config
+);
+
+/** Zend_Layout */
+require_once "Zend/Layout.php";
+
+Zend_Layout::startMvc("../application/layouts");
+
+$front->setParam("rendConfig", $config)
+      ->setParam("rendFactoryLoader", $factoryLoader)
+      ->throwExceptions($config->displayErrors);
+
+$front->getDispatcher()
+      ->setParam("rendConfig", $config)
+      ->setParam("rendFactoryLoader", $factoryLoader);
+
+$front->addControllerDirectory("../application/controllers", "default")
+      ->addModuleDirectory("../application/modules");
