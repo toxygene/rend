332ccfad8f4cc8f24de6bf6a8e5b3141f0a07644
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2009-01-20 04:05:15 +0000

    PHPDoc updates

--- source/library/Rend/Factory/AuthStorage/Session.php
+++ source/library/Rend/Factory/AuthStorage/Session.php
@@ -1,6 +1,22 @@
 <?php
 /**
+ * Rend
  *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.rendframework.com/license.html
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to justin.hendrickson+rend@gmail.com so I can send you a copy immediately.
+ *
+ * @category Rend
+ * @package Factory
+ * @copyright 2008 Justin Hendrickson
+ * @license http://www.rendframework.com/license.html New BSD License
+ * @link http://www.rendframework.com/
+ * @since 2.0.0
+ * @version $Id$
  */
 
 /** Rend_Base_Abstract */
@@ -10,7 +26,8 @@ require_once "Rend/Base/Abstract.php";
 require_once "Rend/Factory/AuthStorage/Interface.php";
 
 /**
- *
+ * @category Rend
+ * @package Factory
  */
 class Rend_Factory_AuthStorage_Session extends Rend_Base_Abstract implements Rend_Factory_AuthStorage_Interface
 {
