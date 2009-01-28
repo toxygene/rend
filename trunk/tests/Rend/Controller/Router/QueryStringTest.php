0f1ef45ab7955b4ceb16999574cd3c405beb1c5f
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2009-01-28 02:45:02 +0000

    Added unit tests for the query string router

--- trunk/tests/Rend/Controller/Router/QueryStringTest.php
+++ trunk/tests/Rend/Controller/Router/QueryStringTest.php
@@ -28,4 +28,72 @@ require_once "Rend/Controller/Router/QueryString.php";
  */
 class Rend_Controller_Router_QueryStringTest extends PHPUnit_Framework_TestCase
 {
+
+    protected $_router;
+
+    public function setUp()
+    {
+        $this->_router = new Rend_Controller_Router_QueryString();
+    }
+
+    public function testAssemblingAddsToParams()
+    {
+        $this->_router->setParams(array(
+            "one" => "two",
+            "three" => array(
+                "four",
+                "five"
+            )
+        ));
+
+        $this->assertEquals(
+            "?one=two&three%5B0%5D=four&three%5B1%5D=five&six=seven",
+            $this->_router->assemble(array("six" => "seven"))
+        );
+    }
+
+    public function testAssemblingWithResetIgnoresParams()
+    {
+        $this->_router->setParams(array(
+            "one" => "two",
+            "three" => array(
+                "four",
+                "five"
+            )
+        ));
+
+        $this->assertEquals(
+            "?six=seven",
+            $this->_router->assemble(array("six" => "seven"), null, true)
+        );
+    }
+
+    /**
+     *
+     */
+    public function testRoutingIncludesParams()
+    {
+        $this->_router->setParams(array(
+            "one" => "two",
+            "three" => array(
+                "four",
+                "five"
+            )
+        ));
+
+        $request = new Zend_Controller_Request_Http("http://localhost/?six=seven");
+
+        $this->assertEquals(
+            array(
+                "one" => "two",
+                "three" => array(
+                    "four",
+                    "five"
+                ),
+                "six" => "seven"
+            ),
+            $this->_router->route($request)
+        );
+    }
+
 }
