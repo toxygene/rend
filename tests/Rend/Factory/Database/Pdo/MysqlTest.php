fabc2bb5240217aa21e8aae0b3b65aaac9f18a82
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2009-02-07 03:28:53 +0000

    Added tests for PDO mysql

--- tests/Rend/Factory/Database/Pdo/MysqlTest.php
+++ tests/Rend/Factory/Database/Pdo/MysqlTest.php
@@ -28,4 +28,111 @@ require_once "Rend/Factory/Database/Pdo/Mysql.php";
  */
 class Rend_Factory_Database_Pdo_MysqlTest extends PHPUnit_Framework_TestCase
 {
+
+    private $_factory;
+
+    public function setUp()
+    {
+        $this->markTestSkipped();
+
+        $this->_factory = new Rend_Factory_Database_Pdo_Mysql();
+
+        $this->_factory
+             ->setPassword('password')
+             ->setSchema('test')
+             ->setUsername('username');
+    }
+
+    public function testAllowLocalInfileIsConfigurable()
+    {
+        $database = $this->_factory
+                         ->setAllowLocalInfile(true)
+                         ->create();
+
+        $this->assertEquals(
+            true,
+            $database->getConnection()
+                     ->getAttribute(PDO::MYSQL_ATTR_LOCAL_INFILE)
+        );
+    }
+
+    public function testInitCommandIsConfigurable()
+    {
+        $database = $this->_factory
+                         ->setInitCommand('test')
+                         ->create();
+
+        $this->assertEquals(
+            'test',
+            $database->getConnection()
+                     ->getAttribute(PDO::MYSQL_ATTR_INIT_COMMAND)
+        );
+    }
+
+    public function testMaximumBufferSizeIsConfigurable()
+    {
+        $database = $this->_factory
+                         ->setMaximumBufferSize(1)
+                         ->create();
+
+        $this->assertEquals(
+            1,
+            $database->getConnection()
+                     ->getAttribute(PDO::MYSQL_ATTR_MAX_BUFFER_SIZE)
+        );
+    }
+
+    public function testReadDefaultFileIsConfigurable()
+    {
+        $database = $this->_factory
+                         ->setDefaultReadFile('asdf')
+                         ->create();
+
+        $this->assertEquals(
+            'asdf',
+            $database->getConnection()
+                     ->getAttribute(PDO::MYSQL_ATTR_READ_DEFAULT_FILE)
+        );
+    }
+
+    public function testReadDefaultGroupIsConfigurable()
+    {
+        $database = $this->_factory
+                         ->setDefaultReadGroup('asdf')
+                         ->create();
+
+        $this->assertEquals(
+            'asdf',
+            $database->getConnection()
+                     ->getAttribute(PDO::MYSQL_ATTR_READ_DEFAULT_GROUP)
+        );
+    }
+
+    public function testUnixSocketIsConfigurable()
+    {
+        $database = $this->_factory
+                         ->setUnixSocket('asdf')
+                         ->create();
+
+        $this->markTestIncomplete();
+    }
+
+    public function testUseBufferedQueriesIsConfigurable()
+    {
+        $database = $this->_factory
+                         ->setUseBufferedQueries(true)
+                         ->create();
+
+        $this->markTestIncomplete();
+    }
+
+    public function testUseDirectQueriesIsConfigurable()
+    {
+        $database = $this->_factory
+                         ->setUseDirectQueries(true)
+                         ->create();
+
+        $this->markTestIncomplete();
+    }
+
 }
