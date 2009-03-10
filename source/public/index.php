3392f8c1a8154df15d77cd27fe579903ad7157b1
justin.hendrickson <justin.hendrickson@7435d909-944c-0410-9d3e-6162093c6d24>
2009-03-10 15:05:29 +0000

    Errors now return 500 HTTP response codes.

--- source/public/index.php
+++ source/public/index.php
@@ -7,6 +7,7 @@ try {
     Zend_Controller_Front::getInstance()
                          ->dispatch();
 } catch (Exception $e) {
+    header("HTTP/1.0 500 Internal Server Error");
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
